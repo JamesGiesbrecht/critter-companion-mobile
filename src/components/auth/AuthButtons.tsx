d4241ab81489668eea8795068f197080d2f1cb37
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-06-29 18:10:31 -0500

    Moving buttons to separate component and adding toggle to switch Forms

---
+++ src/components/auth/AuthButtons.tsx
@@ -0,0 +1,90 @@
+import React, { FC, useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Button, Icon } from 'react-native-elements'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+import Centered from 'src/components/ui/Centered'
+
+interface Props {
+  type: 'signIn' | 'signUp'
+}
+
+const AuthButtons: FC<Props> = ({ type }) => {
+  const insets = useSafeAreaInsets()
+  const [buttonType, setButtonType] = useState(type)
+  const handleLogInWithApple = () => console.log('Logging in with Apple')
+  const handleLogInWithGoogle = () => console.log('Logging in with Google')
+  const handleLogInWithFacebook = () => console.log('Logging in with Facebook')
+  const handleLogInWithEmail = () => console.log('Logging in with Email')
+
+  const handleToggleType = () => setButtonType((prev) => (prev === 'signIn' ? 'signUp' : 'signIn'))
+
+  const getButtonText = (provider: string) =>
+    `${buttonType === 'signIn' ? 'Sign in' : 'Sign up'} with ${provider}`
+
+  return (
+    <View style={styles.screen}>
+      <Centered defaultView style={styles.screen}>
+        <Button
+          containerStyle={styles.button}
+          buttonStyle={{ backgroundColor: 'black' }}
+          title={getButtonText('Apple')}
+          icon={<Icon iconStyle={styles.buttonIcon} name="apple1" type="antdesign" />}
+          onPress={handleLogInWithApple}
+        />
+        <Button
+          containerStyle={styles.button}
+          buttonStyle={{ backgroundColor: '#3F81EC' }}
+          title={getButtonText('Google')}
+          icon={<Icon iconStyle={styles.buttonIcon} name="google" type="antdesign" />}
+          onPress={handleLogInWithGoogle}
+        />
+        <Button
+          containerStyle={styles.button}
+          buttonStyle={{ backgroundColor: '#3F5AA8' }}
+          title={getButtonText('Facebook')}
+          icon={<Icon iconStyle={styles.buttonIcon} name="facebook" type="font-awesome" />}
+          onPress={handleLogInWithFacebook}
+        />
+        <Button
+          containerStyle={styles.button}
+          buttonStyle={{ backgroundColor: '#388e3c' }}
+          title={getButtonText('Email')}
+          icon={<Icon iconStyle={styles.buttonIcon} name="ios-mail" type="ionicon" />}
+          onPress={handleLogInWithEmail}
+        />
+      </Centered>
+      <Button
+        containerStyle={{ alignItems: 'center', paddingBottom: insets.bottom + 5 }}
+        buttonStyle={[styles.button, { backgroundColor: 'white' }]}
+        titleStyle={{ color: 'black' }}
+        title={
+          buttonType === 'signIn'
+            ? "Don't have an account? Sign Up."
+            : 'Already have an account? Sign In.'
+        }
+        onPress={handleToggleType}
+      />
+    </View>
+  )
+}
+
+export default AuthButtons
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+  button: {
+    width: 300,
+    marginTop: 20,
+  },
+  buttonIcon: {
+    marginRight: 10,
+    fontSize: 20,
+    color: 'white',
+  },
+  typeToggleContainer: {
+    alignItems: 'center',
+  },
+})
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,72 +1,25 @@
 import React from 'react'
 import { StyleSheet, ImageBackground } from 'react-native'
-import { Button, Icon } from 'react-native-elements'
 
-import { Text } from 'src/components/Themed'
-import Centered from 'src/components/ui/Centered'
+import AuthButtons from 'src/components/auth/AuthButtons'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const backgroundImage = require('src/assets/images/background.png')
 
 const LoginScreen = () => {
-  const handleLogInWithApple = () => console.log('Logging in with Apple')
-  const handleLogInWithGoogle = () => console.log('Logging in with Google')
-  const handleLogInWithFacebook = () => console.log('Logging in with Facebook')
-  const handleLogInWithEmail = () => console.log('Logging in with Email')
-
   return (
     <ImageBackground
       source={backgroundImage}
       style={styles.backgroundImage}
       imageStyle={{ resizeMode: 'repeat' }}>
-      <Centered defaultView>
-        <Button
-          containerStyle={styles.button}
-          buttonStyle={{ backgroundColor: 'black' }}
-          title="Log in with Apple"
-          icon={<Icon iconStyle={styles.buttonIcon} name="apple1" type="antdesign" />}
-          onPress={handleLogInWithApple}
-        />
-        <Button
-          containerStyle={styles.button}
-          buttonStyle={{ backgroundColor: '#3F81EC' }}
-          title="Log in with Google"
-          icon={<Icon iconStyle={styles.buttonIcon} name="google" type="antdesign" />}
-          onPress={handleLogInWithGoogle}
-        />
-        <Button
-          containerStyle={styles.button}
-          buttonStyle={{ backgroundColor: '#3F5AA8' }}
-          title="Log in with Facebook"
-          icon={<Icon iconStyle={styles.buttonIcon} name="facebook" type="font-awesome" />}
-          onPress={handleLogInWithFacebook}
-        />
-        <Button
-          containerStyle={styles.button}
-          buttonStyle={{ backgroundColor: '#388e3c' }}
-          title="Log in with Email"
-          icon={<Icon iconStyle={styles.buttonIcon} name="ios-mail" type="ionicon" />}
-          onPress={handleLogInWithEmail}
-        />
-      </Centered>
+      <AuthButtons type="signUp" />
     </ImageBackground>
   )
 }
 
 export default LoginScreen
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
-    // resizeMode: 'cover',
-  },
-  button: {
-    width: 300,
-    marginTop: 20,
-  },
-  buttonIcon: {
-    marginRight: 10,
-    fontSize: 20,
-    color: 'white',
   },
 })
